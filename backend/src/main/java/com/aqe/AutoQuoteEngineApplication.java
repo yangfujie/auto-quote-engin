73907eff9b8d90c1e52e301d28c13a67669bc086
Hunter-yang <yangfujie@legend.tech>
2026-07-12 22:01:32 +0800

    启动类

--- backend/src/main/java/com/aqe/AutoQuoteEngineApplication.java
+++ backend/src/main/java/com/aqe/AutoQuoteEngineApplication.java
@@ -1,4 +1,14 @@
+// AutoQuoteEngineApplication.java
 package com.aqe;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class AutoQuoteEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoQuoteEngineApplication.class, args);
+    }
 }
